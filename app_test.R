613a4cee71a04a0970d826b6ff51075df5830baf
Shiva Jena <ssjena89@student.ubc.ca>
2022-03-16 12:40:29 -0700

    Modularize and add color themes to plots

--- app_test.R
+++ app_test.R
@@ -12,10 +12,11 @@ library(purrr)
 library(tibble)
 library(knitr)
 library(htmlTable)
+library(ggthemes)
 
 # Read in data
-traits_raw_df <- read_csv("data/breed_traits.csv")
-breed_rank_raw_df <- read_csv("data/breed_rank.csv")
+traits_raw_df <- readr::read_csv("data/breed_traits.csv")
+breed_rank_raw_df <- readr::read_csv("data/breed_rank.csv")
 
 # For traits list in checklist input
 traits_list_full <- traits_raw_df %>%
@@ -25,70 +26,226 @@ traits_list_full <- traits_raw_df %>%
 # Setup app and layout/frontend
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-app$layout(
-  dbcContainer(
+
+# 1. VARIABLES AND CONTAINERS DEFINITIONS
+# Header
+header <- htmlH1(
     list(
-      htmlH1('DoggoDash'),
-      dccGraph(id='plot'), 
-      dbcRow(
-        list(
-          dbcCol(
-            list(
-              htmlLabel('Choose Dog traits you like and dislike'),
-              dccDropdown(
-                id = 'traits-widget',
-                options = purrr::map(traits_list_full, function(value)
-                  list(label = value, value = value)),
-                value = traits_list_full[1:4],
-                multi = TRUE
-              )
-            )
-          ),
-          dbcCol(
-            list(
-              htmlLabel('Weight for traits you like'),
-              dccSlider(
-                id = "xslider_like",
-                min = 0,
-                max = 2.5,
-                step = 0.5,
-                marks = list(
-                  "0" = "0",
-                  "0.5" = "0.5",
-                  "1" = "1",
-                  "1.5" = "1.5",
-                  "2" = "2",
-                  "2.5" = "2.5"
-                ),
-                value = 1
-              ),
-              htmlLabel('Weight for traits you dislike'),
-              dccSlider(
-                id = "xslider_dislike",
-                min = -2.5,
-                max = 0,
-                step = 0.5,
-                marks = list(
-                  "-2.5" = "-2.5",
-                  "-2" = "-2",
-                  "-1.5" = "-1.5",
-                  "-1" = "-1",
-                  "-0.5" = "-0.5",
-                  "0" = "0"
-                ),
-                value = -0.5
-              )
-            )
-          )
-        )
-      )
+      'Doggodash' 
+    ),
+    style=list(
+      color='white', 
+      fontSize=40,
+      backgroundColor='Indigo',
+      textAlign='center'
+    )
+)
+
+# Row-1
+# Col-1: traits-widget  dropdown
+traits_widget <- list(
+  htmlLabel(
+    'Choose Dog traits you like and dislike',
+    style=list(
+      color='Indigo', 
+      fontSize=18,
+      backgroundColor='lavender',
+      width='100%',
+      textAlign='center'
+    )
+  ),
+  dccDropdown(
+    id = 'traits-widget',
+    value = traits_list_full[1:4],
+    options = purrr::map(traits_list_full, function(value)
+      list(label = value, value = value)),
+    placeholder = 'Select Doggo traits',
+    multi = TRUE,
+    style=list(
+      height= '80%',
+      width= '100%',
+      fontSize= 18,
+      backgroundColor= 'white'
     )
   )
 )
 
-# Setup callback and backend
+# Col-2 - weights widget slider
+weights_slider <- list(
+  htmlLabel(
+    'Select weights',
+    style = list( 
+      color= 'Indigo', 
+      fontSize= 18,
+      textAlign= 'center',
+      align= 'top',
+      backgroundColor= 'lavender',
+      width= '100%'
+    )
+  ),
+  
+  htmlLabel(
+    'Weights for traits you like',
+    style = list( 
+      color= 'Indigo', 
+      fontSize= 14,
+      textAlign= 'center'
+    )
+  ),
+  
+  dccSlider(
+    id = "xslider_like",
+    min = 0,
+    max = 2.5,
+    step = 0.5,
+    marks = list(
+      "0" = "0",
+      "0.5" = "0.5",
+      "1" = "1",
+      "1.5" = "1.5",
+      "2" = "2",
+      "2.5" = "2.5"
+    ),
+    value = 1
+  ),
+  
+  htmlBr(),
+  
+  htmlLabel(
+    'Weight for traits you dislike',
+    style = list( 
+      color= 'Indigo', 
+      fontSize= 14,
+      textAlign= 'center'
+    )
+  ),
+  dccSlider(
+    id = "xslider_dislike",
+    min = -2.5,
+    max = 0,
+    step = 0.5,
+    marks = list(
+      "-2.5" = "-2.5",
+      "-2" = "-2",
+      "-1.5" = "-1.5",
+      "-1" = "-1",
+      "-0.5" = "-0.5",
+      "0" = "0"
+    ),
+    value = -0.5
+  ),
+  
+  htmlBr(),
+  
+  htmlLabel(
+    'Higher weights increase scoring on likeable traits and decrease scoring on dislikeable traits (drooling and shedding levels)',
+    style = list( 
+      color= 'Indigo', 
+      fontSize= 14,
+      textAlign= 'center'
+    )
+  )
+)
+
+
+# Col3 - top5dogs recommender Plot
+top5dogs_plot <- list(
+    htmlP(
+      'Top 5 recommended Dog breeds as per chosen traits. Click to view them!',
+      style = list( 
+        color= 'Indigo', 
+        fontSize= 18,
+        textAlign= 'center',
+        backgroundColor= 'lavender',
+        width= '100%'
+      )
+    ),
+    
+    dccGraph(
+      id='top5dogs_plot',
+      style=list(
+        display= 'block',
+        borderWidth= '1', 
+        height= '230px',
+        width= '100%' 
+      )
+    ),
+    
+    htmlP(
+      'Hover over to see 2020 rank and click to see image. To go back, click on browser back button',
+      style = list( 
+        color= 'Indigo', 
+        fontSize= 14,
+        textAlign= 'center'
+      )
+    )
+)
+
+# Row-2
+#Col-1: Table of details on the recommended breeds
+
+
+# 2. APP LAYOUT
+app$layout(
+  dbcContainer(
+    list(
+      # Header
+      header,
+      
+      # Row1 
+      dbcRow(
+        list(
+          # Traits multi-option dropdown widget
+          dbcCol(
+            traits_widget,
+            md=3
+          ),
+          
+          # Weights widget for traits
+          dbcCol(
+            weights_slider,
+            md=3
+          ),
+          
+          # Top 5 recommended dogs plot based on selected traits
+          dbcCol(
+            top5dogs_plot,
+            md=6
+          )
+        )
+      ),
+      
+      # Second row panel
+      dbcRow(
+        #Table container
+        dbcCol(
+          list(
+            htmlIframe(
+              id = 'table',
+              style = list(
+                height = "600px",
+                width = '800px'
+              )
+            )
+          )
+        ),
+        
+        
+      )
+    ),
+    style=list(
+      backgroundColor= 'Beige'
+    ),
+    fluid=TRUE
+  )
+)
+
+
+# 3. CALLBACKS
+
+# Recommended Dogs
 app$callback(
-  output('plot', 'figure'),
+  output('top5dogs_plot', 'figure'),
   list(
     input('traits-widget', 'value'),
     input('xslider_like', 'value'),
@@ -123,10 +280,40 @@ app$callback(
       ggplot(aes(x = score,
                  y = reorder(Breed.x, score))) +
       labs(x = "Score", y = "Breed")  +
-      ggtitle("Top 5 recommended Dog breeds as per chosen traits") +
-      geom_bar(stat = "identity")
+      ggtitle("Your top 5 Dog breeds") +
+      geom_bar(stat = "identity")+
+      ggthemes::scale_color_tableau()
     
     ggplotly(top_5_plot) %>% layout()
+  }
+)
+
+# Table
+app$callback(
+  output(id='table',property='srcDoc'),
+  list(input(id='traits-widget', property='value')),
+  function(traits){
+    traits_df <- tibble::rowid_to_column(traits_raw_df, 'BreedID')
+    breed_rank_df <- tibble::rowid_to_column(breed_rank_raw_df, "BreedID")
+    
+    traits_df['Score'] <- 0
+    
+    selected_traits <- unlist(traits)
+    
+    for (i in 1:length(selected_traits)) {
+      if (length(selected_traits) == 0) {
+        next
+      } else {
+        traits_df['Score'] <- traits_df['Score'] + traits_df[selected_traits[i]]
+      }
+    }
+    
+    merged_df <- merge(traits_df, breed_rank_df, by="BreedID")
+    
+    top_5_df <- merged_df[order(-merged_df$'Score', merged_df$"2020 Rank"),] %>%
+      slice_head(n = 5)
+    
+    return(top_5_df %>% htmlTable)
   }
 )
 
